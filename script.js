c167576de5c6439364e75c9ae2376d3c236a9b23
Gowtham0509 <gowthamanandaraj02@gmail.com>
2026-01-10 23:11:41 +0530

    added two functions

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello world");
+
+
+function getComputerChoice(){
+    let computerChoice = Math.floor(Math.random() * 3);
+    console.log(computerChoice);
+}
+
+function getHumanChoice(){
+    let userChoice = parseInt(prompt("Rock [0], Papers[1] or Scissors[2]??"));
+    console.log(userChoice);
+}
+// getComputerChoice();
+getHumanChoice();
