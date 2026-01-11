3ce2bd8ae0c0eb9ac5661ce7596d39f7811cd4de
Gowtham0509 <gowthamanandaraj02@gmail.com>
2026-01-11 23:23:46 +0530

    completed javascript logic

--- script.js
+++ script.js
@@ -1,13 +1,56 @@
 
+const computerChoices = ["rock", "paper", "scissors"]; 
+
 
 function getComputerChoice(){
     let computerChoice = Math.floor(Math.random() * 3);
     console.log(computerChoice);
+    return computerChoices[computerChoice];
 }
 
 function getHumanChoice(){
-    let userChoice = parseInt(prompt("Rock [0], Papers[1] or Scissors[2]??"));
+    let userChoice = prompt("Rock, Paper or Scissors??").toLowerCase();
     console.log(userChoice);
+    return userChoice;
 }
-// getComputerChoice();
-getHumanChoice();
+
+function playRound(humanChoice, computerChoice, humanScore, computerScore){
+    if(humanChoice === computerChoice){
+        console.log(`Draw! Both chose ${computerChoice}`);
+    }
+    else if(humanChoice === "rock" && computerChoice === "scissors" ||
+            humanChoice === "scissors" && computerChoice === "paper" ||
+            humanChoice === "paper" && computerChoice === "rock"){
+        console.log(`You Won! ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+    }
+    else{
+        console.log(`You Lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    }
+    return [humanScore, computerScore];
+}
+
+function playGame(){
+    let humanScore = 0;
+    let computerScore = 0;
+
+    for(let i = 1; i <= 5; i++){
+        const computerSelection = getComputerChoice();
+        const humanSelection = getHumanChoice();
+       [humanScore, computerScore] = playRound(humanSelection, computerSelection, humanScore, computerScore);
+    }
+
+    if(humanScore > computerScore){
+        console.log(`Human Score: ${humanScore}\nComputer Score: ${computerScore}\nHuman Wins!`);
+    }
+    else if(computerScore > humanScore){
+        console.log(`Human Score: ${humanScore}\nComputer Score: ${computerScore}\nComputer Wins!`);
+    }
+    else{
+        console.log(`Human Score: ${humanScore}\nComputer Score: ${computerScore}\nDraw!!`);
+    }
+    
+}
+
+playGame();
